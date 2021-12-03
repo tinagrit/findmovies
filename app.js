d64b6b1a45a5df6703a3d1de5979bc940e7d9bcf
TinagritProject <62089209+tinagrit@users.noreply.github.com>
2021-12-03 07:40:56 -0800

    perf

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ function to(x) {
   document.getElementById(x).scrollIntoView(true);
 }
 
+function link(x) {
+  location.href = x;
+}
+
 window.onscroll = function() {
     if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 45) {
         document.querySelector("nav.fix").style.top = "0";
